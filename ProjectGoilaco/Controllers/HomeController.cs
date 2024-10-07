ac4618476f0796866ed469fd4db1ce51185e3f8e
hieube13 <hieubm.work@gmail.com>
2024-10-07 08:04:39 +0700

    init project

---
+++ ProjectGoilaco/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectGoilaco.Models;
+using System.Diagnostics;
+
+namespace ProjectGoilaco.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page3()
+        {
+            return View();
+        }
+
+        public IActionResult Page5()
+        {
+            return View();
+        }
+
+        public IActionResult Page6()
+        {
+            return View();
+        }
+
+        public IActionResult Page7()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
